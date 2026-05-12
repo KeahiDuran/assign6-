76988a819e135bcd56b721f8d4e52bfad4eeead6
KeahiDuran <kduran4@my.chemeketa.edu>
2026-05-12 01:38:55 +0000

    May 11, 2026, 6:38 PM

--- main.cpp
+++ main.cpp
@@ -43,7 +43,7 @@ int main()
   );
 
 
-  cout << endl"Task 2: Count the Spades" << endl; 
+  cout << endl << "Task 2: Count the Spades" << endl; 
 
   int spadeCount = count_if( 
     cards.begin(), 
