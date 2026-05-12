2bfffc30506951682ec4ac259221556d9b9293c2
KeahiDuran <kduran4@my.chemeketa.edu>
2026-05-12 03:18:36 +0000

    May 11, 2026, 8:18 PM

--- main.cpp
+++ main.cpp
@@ -158,7 +158,7 @@ int main()
     }
   );
 
-  for (cpmst auto& pair : rankCounts) { 
+  for (const auto& pair : rankCounts) { 
     cout << "Rank " << pair.first << ": " << pair.second << endl;
   }
   return 0;
